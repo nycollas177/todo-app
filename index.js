f100e0e5c62d8b4385b35e4c21096d0c1b528397
Nycollas <cauacarvalho@MacBook-Air-de-Teste.local>
2023-11-25 14:40:43 -0300

    Adição de icone de lua e sol para troca de tema

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const exphbs = require("express-handlebars");
 const app = express();
 app.engine("handlebars",exphbs.engine());
 app.set("view engine", "handlebars")
+app.use(express.static('public'))
 
 app.listen(3000, ()=>{
     console.log("Server rodando na porta 3000, http://localhost:3000")
