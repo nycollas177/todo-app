02f5fae3069ab1e8ad4d03e62219ca0569c64cb2
Nycollas <cauacarvalho@MacBook-Air-de-Teste.local>
2023-11-25 12:51:52 -0300

    Primeira execução do servidor

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 const express = require("express");
 const app = express();
+
+app.listen(3000, ()=>{
+    console.log("Server rodando na porta 3000, http://localhost:3000")
+});
+
+app.get("/",(req,res)=>{
+    res.send("Hello World!")
+})
