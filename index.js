34c0ec2ac1d481eb38e90f49276f1c3588a6ccb1
Nycollas <cauacarvalho@MacBook-Air-de-Teste.local>
2023-11-25 12:57:37 -0300

    Instalação do handlebars e do nodemon

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require("express");
+const exphbs = require("express-handlebars");
+
+
 const app = express();
+app.engine("handlebars",exphbs.engine());
+app.set("view engine", "handlebars")
 
 app.listen(3000, ()=>{
     console.log("Server rodando na porta 3000, http://localhost:3000")
