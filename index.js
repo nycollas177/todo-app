0e8dd0dd9290e3f6d391d0dfa3204df3cb46e24f
Nycollas <cauacarvalho@MacBook-Air-de-Teste.local>
2023-11-27 00:19:27 -0300

    Salvando uma tarefa no banco de dados

--- index.js
+++ index.js
@@ -8,6 +8,28 @@ app.engine("handlebars",exphbs.engine());
 app.set("view engine", "handlebars")
 app.use(express.static('public'))
 
+app.use(express.urlencoded({
+    extended: true
+}))
+app.use(express.json())
+
+/* Rotas */
+app.get("/",(req,res)=>{
+    res.render('home')
+})
+app.post("/criar",(req,res)=>{
+    const descricao = req.body.descricao;
+    const completa = 0;
+
+    const sql = `
+        INSERT INTO tarefas(descricao,completa)
+        VALUES ('${descricao}','${completa}')
+    `
+    conexao.query(sql,(erro)=>{
+        if (erro) return console.log(erro);
+        res.redirect('/')
+    })
+})
 
 const conexao = mysql.createConnection({
     host: 'localhost',
@@ -15,10 +37,6 @@ const conexao = mysql.createConnection({
     password: '.caua2023.',
     database: 'todoapp',
     port: 3306
-})
-
-app.get("/",(req,res)=>{
-    res.render('home')
 })
 
 conexao.connect((erro)=>{
